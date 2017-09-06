36f0ccc62d631f3901b3f3bc9d043095bf093cde
Alex Lituma <alejandro_lituma@hotmail.com>
2017-09-06 17:52:09 +0000

    Add styling to form template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     #What if validation does not pass, we need to take that into consideration
     if @article.save
       #do something
-      flash[:notice] = "Article was succesfully created"
+      flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
     else
       #validation failed
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:notice] = "Article was succesfully updated"
+      flash[:success] = "Article was succesfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Article was succesfully deleted"
+    flash[:danger] = "Article was succesfully deleted"
     redirect_to articles_path
   end
   
